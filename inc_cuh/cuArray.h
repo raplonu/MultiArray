8aba48c3ddfef8d2a40321aa06cc0047ec121a39
Julien Bernard <raplonu.jb@gmail.com>
2016-10-11 10:56:45 +0200

    Debug and add licence

--- inc_cuh/cuArray.h
+++ inc_cuh/cuArray.h
@@ -1,220 +1,275 @@
-#pragma once
+/*
+* Copyright (c) 2016 Julien Bernard
+* 
+* Permission is hereby granted, free of charge, to any person obtaining
+* a copy of this software and associated documentation files (the
+* "Software"), to deal in the Software without restriction, including
+* without limitation the rights to use, copy, modify, merge, publish,
+* distribute, sublicense, and/or sell copies of the Software, and to
+* permit persons to whom the Software is furnished to do so, subject to
+* the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included
+* in all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+* OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+* FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+* IN THE SOFTWARE.
+*/
+
+#ifndef MA_CUARRAY_H
+#define MA_CUARRAY_H
+
 #include <iostream>
 #include <cuda_runtime_api.h>
 
 #include "../int/Array.h"
 
-int const INVALIDDEVICE(-1);
-
-struct DeviceId
+namespace ma
 {
-	DeviceId() :
+	static void HandleError(
+		cudaError_t err,
+		const char *file,
+		i32 line
+	){
+		if (err != cudaSuccess) {
+			cerr
+				<< cudaGetErrorString( err )
+				<< " in " << file
+				<< " at line " << line << endl;
+			exit( EXIT_FAILURE );
+		}
+	}
+	
+	#define HANDLE_ERROR( err ) (HandleError( err, __FILE__, __LINE__ ))
+
+	int const INVALIDDEVICE(-1);
+
+	struct DeviceId
+	{
+		int id;
+		
+		DeviceId() :
 			id(INVALIDDEVICE) {}
 
-	DeviceId(int id) :
-			id(id) {}
+		DeviceId(int id)
+			:id(id)
+		{}
 
-	DeviceId(DeviceId const & deviceId) :
-			id(deviceId.id) {}
+		DeviceId(DeviceId const & deviceId)
+			:id(deviceId.id)
+		{}
 
-	bool isValid() const
+		bool isValid() const
+		{
+			return id != INVALIDDEVICE;
+		}
+
+		void set() const
+		{
+			if (isValid())
+				HANDLE_ERROR(cudaSetDevice(id));
+		}
+
+		void operator=(int newId)
+		{
+			id = newId;
+		}
+
+		operator int() const
+		{
+			return id;
+		}
+	};
+
+	template<typename T>
+	struct Device
 	{
-		return id != INVALIDDEVICE;
-	}
+		T* _ptr;
+	    size_t _size;
+	    DeviceId _id;
+		
+		Device():_ptr(nullptr), _size(), _id(){}
 
-	void set() const
-	{
-		if (isValid())
-			cudaSetDevice(id);
-	}
+		Device(size_t nbElement):_ptr(nullptr), _size(nbElement * sizeof(T)), _id()
+	    {
+			allocate();
+	    }
 
-	void operator=(int newId)
-	{
-		id = newId;
-	}
+		Device(size_t nbElement, DeviceId id):_ptr(nullptr), _size(nbElement * sizeof(T)), _id(id)
+	    {
+			allocate();
+	    }
 
-	operator int() const
-	{
-		return id;
-	}
+	    ~Device()
+	    {
+			deallocate();
+	    }
 
-	int id;
-};
+	    void allocate()
+	    {
+			if(_ptr)
+				deallocate();
 
-template<typename T>
-struct Device
-{
-	Device():ptr_(nullptr), size_(), id_(){}
+			_id.set();
+			HANDLE_ERROR(cudaMalloc((void**)_ptr, _size * sizeof(T)));
+	    }
 
-	Device(size_t nbElement):ptr_(nullptr), size_(nbElement * sizeof(T)), id_()
-    {
-        allocate();
-    }
+	    void deallocate()
+	    {
+			if(_ptr)
+			{
+		    	cudaFree(_ptr);
+		    	_ptr = nullptr;
+			}
+	    }
 
-	Device(size_t nbElement, DeviceId id):ptr_(nullptr), size_(nbElement * sizeof(T)), id_(id)
-    {
-        allocate();
-    }
+	    void setDevice(DeviceId newId)
+	    {
+			if(_id != newId)
+			{
+				_id = newId;
+				allocate();
+			}
+	    }
 
-    ~Device()
-    {
-    	deallocate();
-    }
-
-    void allocate()
-    {
-        if(ptr_)
-        	deallocate();
-
-        cudaMalloc<T>(&ptr_, size_, id_);
-    }
-
-    void deallocate()
-    {
-        if(ptr_)
-        {
-            cudaFree(ptr_);
-            ptr_ = nullptr;
-        }
-    }
-
-    void setDevice(DeviceId newId)
-    {
-    	if(id_ != newId)
+		T* ptr() const
     	{
-    		id_ = newId;
-    		allocate();
+        	return _ptr;
     	}
-    }
+		
+	    operator T*() const
+	    {
+			return _ptr;
+	    }
 
-    operator T*() const
-    {
-        return ptr_;
-    }
+		T* operator+(size_t offset) const
+		{
+			return _ptr + offset;
+		}
+	};
 
-    T* operator+(size_t offset) const
-    {
-    	return ptr_ + offset;
-    }
-
-    T* ptr_;
-    size_t size_;
-    DeviceId id_;
-};
-
-template<typename T, typename T2>
-void memCopy(
-		Device<T> & dst, T2 const & src, size_t nbElement,
-		size_t offsetDst, size_t offsetSrc
-){
-	HANDLE_ERROR(
-		cudaMemcpy(
-			dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
-			cudaMemcpyHostToDevice
-	));
-}
-
-template<typename T, typename T2>
-void memCopy(
-		Device<T> & dst, T2 const & src, size_t nbElement,
-		size_t offsetDst, size_t offsetSrc,
-		cudaStream_t stream
-){
-	HANDLE_ERROR(
-		cudaMemcpyAsync(
-			dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
-			cudaMemcpyHostToDevice, stream
-	));
-}
-
-template<typename T1, typename T>
-void memCopy(
-		T1 & dst, Device<T> & src, size_t nbElement,
-		size_t offsetDst, size_t offsetSrc
-){
-	HANDLE_ERROR(
-		cudaMemcpy(
-			dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
-			cudaMemcpyDeviceToHost
-	));
-}
-
-template<typename T1, typename T>
-void memCopy(
-		T1 & dst, Device<T> const & src, size_t nbElement,
-		size_t offsetDst, size_t offsetSrc,
-		cudaStream_t stream
-){
-	HANDLE_ERROR(
-		cudaMemcpyAsync(
-			dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
-			cudaMemcpyDeviceToHost, stream
-	));
-}
-
-template<typename T>
-void memCopy(
-		Device<T> & dst, Device<T> const & src, size_t nbElement,
-		size_t offsetDst, size_t offsetSrc
-){
-	HANDLE_ERROR(
-		cudaMemcpy(
-			dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
-			cudaMemcpyDeviceToDevice
-	));
-}
-
-template<typename T>
-void memCopy(
-		Device<T> & dst, Device<T> const & src, size_t nbElement,
-		size_t offsetDst, size_t offsetSrc,
-		cudaStream_t stream
-){
-	HANDLE_ERROR(
-		cudaMemcpyAsync(
-			dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
-			cudaMemcpyDeviceToDevice, stream
-	));
-}
-
-template<typename T>
-void memFill(
-	Device<T> & dst, T const value, size_t nbElement,
-	size_t offsetDst
-){
-	T *tmpPtr = new T[nbElement];
-    std::fill<T*, T>(tmpPtr, tmpPtr + nbElement, value);
-    memCopy(dst, tmpPtr, nbElement, offsetDst, 0);
-    delete[] tmpPtr;
-}
-
-template<typename T>
-void memFill(
-	Device<T> & dst, T const value, size_t nbElement,
-	size_t offsetDst, cudaStream_t stream
-){
-	T *tmpPtr = new T[nbElement];
-    std::fill(tmpPtr, tmpPtr + nbElement, value);
-    memCopy<Device<T>, T*>(dst, tmpPtr, nbElement, offsetDst, 0, stream);
-}
-
-
-template<typename T>
-class DeviceArray : public BaseArray<T, Device<T>>
-{
-public:
-	using BaseArray<T, Device<T>>::BaseArray;
-
-	void setDevice(DeviceId const newId)
-	{
-		BaseArray<T, Device<T>>::container_->board_.setDevice(newId);
+	template<typename T, typename T2>
+	void memCopy(
+			Device<T> & dst, T2 const & src, size_t nbElement,
+			size_t offsetDst, size_t offsetSrc
+	){
+		HANDLE_ERROR(
+			cudaMemcpy(
+				dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
+				cudaMemcpyHostToDevice
+		));
 	}
 
-	DeviceId deviceId() const
-	{
-		return BaseArray<T, Device<T>>::container_->board_.id_;
+	template<typename T, typename T2>
+	void memCopy(
+			Device<T> & dst, T2 const & src, size_t nbElement,
+			size_t offsetDst, size_t offsetSrc,
+			cudaStream_t stream
+	){
+		HANDLE_ERROR(
+			cudaMemcpyAsync(
+				dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
+				cudaMemcpyHostToDevice, stream
+		));
 	}
-};
 
-template<typename T>
-using HostArray = Array<T>;
+	template<typename T1, typename T>
+	void memCopy(
+			T1 & dst, Device<T> & src, size_t nbElement,
+			size_t offsetDst, size_t offsetSrc
+	){
+		HANDLE_ERROR(
+			cudaMemcpy(
+				dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
+				cudaMemcpyDeviceToHost
+		));
+	}
+
+	template<typename T1, typename T>
+	void memCopy(
+			T1 & dst, Device<T> const & src, size_t nbElement,
+			size_t offsetDst, size_t offsetSrc,
+			cudaStream_t stream
+	){
+		HANDLE_ERROR(
+			cudaMemcpyAsync(
+				dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
+				cudaMemcpyDeviceToHost, stream
+		));
+	}
+
+	template<typename T>
+	void memCopy(
+			Device<T> & dst, Device<T> const & src, size_t nbElement,
+			size_t offsetDst, size_t offsetSrc
+	){
+		HANDLE_ERROR(
+			cudaMemcpy(
+				dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
+				cudaMemcpyDeviceToDevice
+		));
+	}
+
+	template<typename T>
+	void memCopy(
+			Device<T> & dst, Device<T> const & src, size_t nbElement,
+			size_t offsetDst, size_t offsetSrc,
+			cudaStream_t stream
+	){
+		HANDLE_ERROR(
+			cudaMemcpyAsync(
+				dst + offsetDst, src + offsetSrc, nbElement * sizeof(T),
+				cudaMemcpyDeviceToDevice, stream
+		));
+	}
+
+	template<typename T>
+	void memFill(
+		Device<T> & dst, T const value, size_t nbElement,
+		size_t offsetDst
+	){
+		T *tmpPtr = new T[nbElement];
+	    std::fill<T*, T>(tmpPtr, tmpPtr + nbElement, value);
+	    memCopy(dst, tmpPtr, nbElement, offsetDst, 0);
+	    delete[] tmpPtr;
+	}
+
+	template<typename T>
+	void memFill(
+		Device<T> & dst, T const value, size_t nbElement,
+		size_t offsetDst, cudaStream_t stream
+	){
+		T *tmpPtr = new T[nbElement];
+	    std::fill(tmpPtr, tmpPtr + nbElement, value);
+	    memCopy<Device<T>, T*>(dst, tmpPtr, nbElement, offsetDst, 0, stream);
+	}
+
+
+	template<typename T>
+	class DeviceArray : public BaseArray<T, Device<T>>
+	{
+	public:
+		using BaseArray<T, Device<T>>::BaseArray;
+
+		void setDevice(DeviceId const newId)
+		{
+			BaseArray<T, Device<T>>::container_->board_.setDevice(newId);
+		}
+
+		DeviceId deviceId() const
+		{
+			return BaseArray<T, Device<T>>::container_->board_.id_;
+		}
+	};
+
+	template<typename T>
+	using HostArray = Array<T>;
+
+}
+
+#endif //MA_CUARRAY_H
