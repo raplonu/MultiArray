15efed04861c40e7c4e98d3b1aa580011a1afe71
raplonu <raplonu.jb@gmail.com>
2017-09-11 07:03:59 -0700

    remove old code

--- inc/ma/data/containerFunction.h
+++ inc/ma/data/containerFunction.h
@@ -11,53 +11,22 @@ namespace ma
 {
     namespace data
     {
-        // template
-        // <
-        //     typename T
-        //     // , typename AllocatorDst, typename AllocatorSrc,
-        //     // typename Enabled = void
-        // >
-        // struct MemCopy;
 
-        // template
-        // <
-        //     typename T
-        //     // , typename Alloc,
-        //     // typename Enabled = void
-        // >
-        // struct MemFill;
+        template<typename T> //force template property
+        void copy(T * dst, T const * src, SizeT size)
+        {
+            std::memcpy(dst, src, size * sizeof(T));
+        }
 
-        // template<typename T>
-        // struct MemCopy//<T, detail::DefaultAlloc<T>, detail::DefaultAlloc<T>>
-        // {
-            template<typename T> //force template property
-            void copy(T * dst, T const * src, SizeT size)
-            {
-                std::memcpy(dst, src, size * sizeof(T));
-            }
-        // };
+        template<typename T> //force template property
+        void fill(T * dst, T val, SizeT size)
+        {
+            std::fill_n(dst, size, val);
+        }
 
-        // template<typename T>
-        // struct MemFill//<T, detail::DefaultAlloc<T>>
-        // {
-            template<typename T> //force template property
-            void fill(T * dst, T val, SizeT size)
-            {
-                std::fill_n(dst, size, val);
-            }
-        // };
-        // template<typename T, typename DST> //force template property
-        // void fill(DST dst, T val, SizeT size)
-        // {
-        //     //thrust::fill_n(dst, size, val);
-        // }
-
-        template<
-            //typename AllocatorDst, typename AllocatorSrc,
-            typename T, typename DST, typename... Args>
+        template<typename T, typename DST, typename... Args>
         void setMem(DST dst, T val, SizeT size, Args... args)
         {
-            //MemFill<T/*, AllocatorDst*/>::template
             fill<T>(
                 dst, val, size, args...
             );
@@ -65,15 +34,13 @@ namespace ma
 
         template
         <
-            //typename AllocatorDst, typename AllocatorSrc,
-            typename T, typename DST, typename SRC, typename = typename std::enable_if<!std::is_same<SRC, T>::value>::type, typename... Args
+            typename T, typename DST, typename SRC,
+            typename = typename std::enable_if<!std::is_same<SRC, T>::value>::type,
+            typename... Args
         >
         void setMem(DST dst, SRC src, SizeT size, Args... args)
         {
-            //MemCopy<T/*, AllocatorDst, AllocatorSrc*/>::template
-            copy<T>(
-                dst, src, size, args...
-            );
+            copy<T>(dst, src, size, args...);
         }
 
 
