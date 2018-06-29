9b0edc76d4e8a13279bf4d4987ed56ec64b38304
Julien Bernard <raplonu.jb@gmail.com>
2018-06-29 19:07:18 +0200

    support range

---
+++ inc/ma/range/LinearRange.h
@@ -0,0 +1,15 @@
+#ifndef MA_RANGE_LINEAR_RANGE_H
+#define MA_RANGE_LINEAR_RANGE_H
+
+namespace ma
+{
+    namespace range
+    {
+        class LinearRange
+        {
+
+        };
+    }
+}
+
+#endif //MA_RANGE_LINEAR_RANGE_H
