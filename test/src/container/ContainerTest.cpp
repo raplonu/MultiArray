b0d6b4b323fa5320c7e89280c33d330d7355da99
Julien Bernard <raplonu.jb@gmail.com>
2018-06-29 17:59:00 +0200

    move test

--- test/src/container/ContainerTest.cpp
+++ test/src/container/ContainerTest.cpp
@@ -1,50 +1,50 @@
 #include <gtest/gtest.h>
 
-#include <ma/container/SharedContainer.h>
+#include <ma/container/Container.h>
 
 using namespace ma;
 using namespace ma::container;
 
 namespace
 {
-    TEST(SharedContainerTest, SimpleConstructor)
+    TEST(ContainerTest, SimpleConstructor)
     {
-        SharedContainer<int> c;
+        Container<int> c;
 
         EXPECT_EQ(c.size(), 0);
         EXPECT_EQ(c.data(), nullptr);
     }
 
-    TEST(SharedContainerTest, ConstructorWithSize)
+    TEST(ContainerTest, ConstructorWithSize)
     {
         SizeT size = 100;
 
-        SharedContainer<int> c(size);
+        Container<int> c(size);
 
         EXPECT_EQ(c.size(), size);
         EXPECT_NE(c.data(), nullptr);
     }
 
-    TEST(SharedContainerTest, CopyConstructor)
+    TEST(ContainerTest, CopyConstructor)
     {
         SizeT size = 100;
 
-        SharedContainer<int> c1(size);
-        SharedContainer<int> c2(c1);
+        Container<int> c1(size);
+        Container<int> c2(c1);
 
         EXPECT_EQ(c2.size(), size);
         EXPECT_NE(c2.data(), nullptr);
     }
 
-    TEST(SharedContainerTest, MoveConstructor)
+    TEST(ContainerTest, MoveConstructor)
     {
         SizeT size = 100;
 
-        SharedContainer<int> c1(size);
+        Container<int> c1(size);
 
         auto ptr = c1.data();
 
-        SharedContainer<int> c2(std::move(c1));
+        Container<int> c2(std::move(c1));
 
         EXPECT_EQ(c2.size(), size);
         EXPECT_EQ(c2.data(), ptr);
