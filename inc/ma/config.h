0b937f03b8499d253c0cc0f9dc114dcc72f7b7aa
Julien Bernard <raplonu.jb@gmail.com>
2018-07-04 15:27:52 +0200

    support for assert

--- inc/ma/config.h
+++ inc/ma/config.h
@@ -1,7 +1,19 @@
 #ifndef MA_CONFIG_H
 #define MA_CONFIG_H
 
+//#define NDEBUG
+#include <cassert>
+
 #define MA_CXX14 (__cplusplus >= 201402L)
 #define MA_CXX17 (__cplusplus >= 201703L)
+
+/**
+ * Multi line constexpr only enable for c++ 14 and upper, else disabled
+ **/
+#if MA_CXX14
+#define MLCONSTEXPR constexpr
+#else
+#define MLCONSTEXPR
+#endif
 
 #endif //MA_CONFIG_H
